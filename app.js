1cead6622b4d07886f652a7630249238a0c4b0c6
mayank4747 <sugarddy.673@gmail.com>
2021-10-15 10:31:01 +0530

    changes for heroku

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.delete('/todo/:id' , async(req,res)=>{
     res.redirect('/');
 })
 
-
-app.listen(3000, () => {
+const  port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("listenig to server");
 })
